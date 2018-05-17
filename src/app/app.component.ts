4f0f187310514dbfd81673706080e26431ad449f
Kuba Szalaty <nnaisur@gmail.com>
2018-05-17 21:13:58 +0200

    Add window ref service and detecting browserlang

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LotteryService } from './lottery.service';
 import { Draw } from './types';
 import { Observable } from 'rxjs';
+import { WindowRefService } from './window-ref.service';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,10 @@ export class AppComponent implements OnInit {
   draws$: Observable<Draw[]>;
   isLangDE = false;
 
-  constructor(private lotteryService: LotteryService) {}
+  constructor(private lotteryService: LotteryService, private windowRef: WindowRefService) {}
 
   ngOnInit(): void {
     this.draws$ = this.lotteryService.getDraws();
+    this.isLangDE = this.windowRef.browserLang === 'de';
   }
 }
